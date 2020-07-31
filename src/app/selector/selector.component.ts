96ad9e0748f26a8ee883320ce1e32d956eb38a57
Anton <bakunin.abolnikov@gmail.com>
2020-07-31 22:41:04 +0300

    add moment.pipe

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {DateService} from '../shared/date.service';
+import { Component } from '@angular/core';
+import { DateService } from '../shared/date.service';
 
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
   styleUrls: ['./selector.component.css']
 })
-export class SelectorComponent implements OnInit {
+export class SelectorComponent {
 
   constructor(private dateServicw: DateService) { }
 
-  ngOnInit(): void {
-  }
 
 }
